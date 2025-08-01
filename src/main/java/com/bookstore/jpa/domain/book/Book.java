69aaef81995e753c125b3a006df8e079a4e3dd66
lohanvictor <lohan.victor@hotmail.com>
2025-08-01 17:34:43 -0400

    feat: creating Author, Book, Publisher and Review class

---
+++ src/main/java/com/bookstore/jpa/domain/book/Book.java
@@ -0,0 +1,34 @@
+package com.bookstore.jpa.domain.book;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "books")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Book implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+}
---
+++ src/main/java/com/bookstore/jpa/domain/review/Review.java
@@ -0,0 +1,34 @@
+package com.bookstore.jpa.domain.review;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "reviews")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Review implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String comment;
+}
